f4d076a6f3b66ae653b8c681c1815e343e2caa2b
Alimzhan Khalelov <alimzhan.khalelov@gmail.com>
2026-06-01 13:55:55 +0500

    fix: update BubbleMenu import for Tiptap v3

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useEditor, EditorContent, BubbleMenu } from '@tiptap/react';
+import { useEditor, EditorContent } from '@tiptap/react';
+import { BubbleMenu } from '@tiptap/react/menus';
 import StarterKit from '@tiptap/starter-kit';
 import { Markdown } from 'tiptap-markdown';
 import Link from '@tiptap/extension-link';
